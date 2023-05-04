ff2605fa0b8f8d1e19feee7f624f7f8daa972846
Yassine ICHIRRI <116758619+JstAnID@users.noreply.github.com>
2023-05-05 00:34:07 +0100

    Update Scrap_py.py

--- Scrap_py.py
+++ Scrap_py.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests 
 
-url = "http://www.wikicfp.com/cfp/"
+url = "http://www.wikicfp.com/cfp/home"
 response = requests.get(url)
 html = response.content
 soup = bs(html, 'lxml')
@@ -11,5 +11,7 @@ rows = table.find_all('tr')[1:]
 
 for row in rows:
     cols = row.find_all('td')
-    title = cols[2].text.strip()
-    print(title)
+    title0 = cols[0].text.strip()
+    title1 = cols[1].text.strip()
+    title2 = cols[2].text.strip()
+    print(title0,title1,title2 ,"\n")
